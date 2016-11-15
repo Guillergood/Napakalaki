bdbb2e39ede02f9643c4fb07380c6c8318e7b94d
Guillermo <Guillermo@Sobremesa>
2016-11-15 11:40:14 +0100

    Practica 2 - Clase Player, enum CombatResult, singleton Napakalaki, 	singleton CardDealer, cambios en Monster, cambios en BadConsequence 	(MAXTREASURES), Clase Treasure.

---
+++ src/napakalaki/Treasure.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Guillermo
+ */
+public class Treasure {
+    
+    private String name;
+    private int bonus;
+    
+    public Treasure(String n, int bonus_p, TreasureKind t){
+        
+    }
+    public String getName(){
+        return(name);
+    }
+    public int getBonus(){
+        return(bonus);
+    }
+    public TreasureKind getType(){
+        
+    }
+    
+}
